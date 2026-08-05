145f021af4b3c9bc77e2d05ab4076ee8aa5b02b8
Naman Jesani <namanjesani2464@gmail.com>
2026-08-06 01:42:56 +0530

    otp validation

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,4 +1,4 @@
-exports.registerUser= (req, res) => {
+exports.registerUser= async (req, res) => {
  try{
    const { username, email, password } = req.body;
    if(!username || !email || !password){
@@ -10,8 +10,27 @@ exports.registerUser= (req, res) => {
    if(!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)){
      return res.status(400).json({ message: 'Please provide a valid email address' });
    }
-   
- catch (error) {
+      const existingUser = await User.findOne({ email });
+    if(existingUser){
+      return res.status(400).json({ message: 'Email already in use' });
+    }
 
+    const otp= Math.floor(100000 + Math.random() * 900000).toString();
+    const otpExpiry= new Date(Date.now() + 10 * 60 * 1000); // OTP valid for 10 minutes;
+
+
+   const user = await User.create({ username, email, password,otp,otpExpiry });
+  res.status(201).json({ message: 'User registered successfully', user });
+
+   // otp validation 
+   try{
+     await sendEmail(email, 'Your OTP Code for AI COLDMAIL GENERATOR', `Your OTP code is: ${otp}. It will expire in 10 minutes.`);
+   }
+   catch(error){
+    console.log({ message: 'Error sending OTP email', error: error.message });
+   }
+
+ }
+ catch(error) {
 }
 }
